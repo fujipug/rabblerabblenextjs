4a333a13e854258a2427862894936f537dc948ca
Primata <lucasvinzon@gmail.com>
2023-08-07 17:39:22 -0300

    add avvy domain and custom connect buttom

--- utils/wagmi-config.ts
+++ utils/wagmi-config.ts
@@ -1,25 +1,31 @@
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { avalanche, mainnet, polygonMumbai } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 import {
-  polygonMumbai
-} from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-import { coinbaseWallet, coreWallet, injectedWallet, metaMaskWallet, rabbyWallet, rainbowWallet, walletConnectWallet } from '@rainbow-me/rainbowkit/wallets';
-import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+  coinbaseWallet,
+  coreWallet,
+  injectedWallet,
+  metaMaskWallet,
+  rabbyWallet,
+  rainbowWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
 
-const projectId = '5ebeded86a2892064a847992b9c2ab4b'; // connect cloud wallet
-const appName = 'Rabble Rabble';
+const projectId = "5ebeded86a2892064a847992b9c2ab4b"; // connect cloud wallet
+const appName = "Rabble Rabble";
 export const { chains, publicClient } = configureChains(
-  [polygonMumbai],
+  [polygonMumbai, avalanche],
   [
-    alchemyProvider({ apiKey: 'Fh3FbYFfhvNgUpnJ-pzgRoIFRunkpA08' }),
-    publicProvider()
-  ]
+    alchemyProvider({ apiKey: "Fh3FbYFfhvNgUpnJ-pzgRoIFRunkpA08" }),
+    publicProvider(),
+  ],
 );
 
 const connectors = connectorsForWallets([
   {
-    groupName: 'Recommended',
+    groupName: "Recommended",
     wallets: [
       metaMaskWallet({ projectId, chains }),
       rabbyWallet({ chains }),
@@ -35,5 +41,5 @@ const connectors = connectorsForWallets([
 export const wagmiConfig = createConfig({
   autoConnect: true,
   connectors,
-  publicClient
-})
+  publicClient,
+});
