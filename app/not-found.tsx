9dfd5718521bb59edfaf575dd3e07ab57b4411c7
fujipug <lane.fujikado@gmail.com>
2023-08-08 13:26:29 -0600

    404 init

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,16 +2,19 @@ import Image from 'next/image'
 
 export function NotFoundPage() {
     return (
-        <div className="h-screen">
-            <Image
-                width={500}
-                height={500}
-                src="/public/images/Quokka_Wave.png"
-                alt="404"
-                layout="fill"
-                objectFit="contain"
-            />
-        </div>
+        <>
+            <div className="h-screen">
+                <Image
+                    src={'/images/Quokka_Wave.png'}
+                    alt="404"
+                    width={400}
+                    height={400}
+                    // layout="fill"
+                    objectFit="contain"
+                />
+                <div>404: Page Not Found</div>
+            </div>
+        </>
     )
 }
 
