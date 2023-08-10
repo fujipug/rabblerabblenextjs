a3a77136f6f05ff158c6cee68128161f2e9fbed0
fujipug <lane.fujikado@gmail.com>
2023-08-09 22:30:51 -0600

    fix countdown

--- components/countdown.tsx
+++ components/countdown.tsx
@@ -27,7 +27,7 @@ export default function Countdown(props: { endTime: Timestamp, size: string }) {
   }, [props.endTime]);
 
   useEffect(() => {
-    setInterval(() => {
+    const countdown = setInterval(() => {
       if (countdownSeconds > 0) {
         countdownSeconds--;
         document.getElementById('seconds')?.style.setProperty('--value', countdownSeconds.toString());
@@ -49,6 +49,8 @@ export default function Countdown(props: { endTime: Timestamp, size: string }) {
         }
       }
     }, 1000);
+
+    return () => clearInterval(countdown);
   }, [countdownTimeRemaining]);
 
   return (
